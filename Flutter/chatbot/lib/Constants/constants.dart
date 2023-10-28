dab7743a4cd319558d4d55b27506139fec791bee
AkashKannan <kannanakash891@gmail.com>
2023-10-28 17:09:53 +0530

    added lib directory

---
+++ Flutter/chatbot/lib/Constants/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+String title = "GridNLP";
+Color cardColor = const Color(0xFF2E2E2E);
+Color scafflodBackgroundColor = const Color(0xFF171717);
---
+++ Flutter/chatbot/lib/Screens/SplashScreen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    //initialization tasks here
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.toNamed('/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.asset(
+          'assets/chatbot.png',
+          width: 150,
+          height: 150,
+        ),
+      ),
+    );
+  }
+}
